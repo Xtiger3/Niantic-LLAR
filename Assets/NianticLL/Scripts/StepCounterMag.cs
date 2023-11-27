2d84c6c82cb8ab2308e6d2faadce43bca4a3e6e0
sssxrf <100052475+sssxrf@users.noreply.github.com>
2023-11-26 20:25:38 -0500

    Update step counter

--- Assets/NianticLL/Scripts/StepCounterMag.cs
+++ Assets/NianticLL/Scripts/StepCounterMag.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PedometerU;
+using TMPro;
 
 public class StepCounterMag : MonoBehaviour
 {
     // Start is called before the first frame update
+    public TextMeshProUGUI StepCounterDisplay;
+    public TextMeshProUGUI DistCounterDisplay;
+
     void Start()
     {
         var pedometer = new Pedometer(OnStep);
 
-        void OnStep(int steps, double distance)
-        {
-            
-        }
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnStep(int steps, double distance)
     {
-        
+        StepCounterDisplay.text = steps.ToString();
+        DistCounterDisplay.text = (distance * 3.28084).ToString("F2") + " ft";
     }
 }
