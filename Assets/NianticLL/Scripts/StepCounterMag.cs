7e09030adeb52bef6456d3866656c0000657f21c
sssxrf <100052475+sssxrf@users.noreply.github.com>
2023-11-26 21:02:19 -0500

    update stepcounter

--- Assets/NianticLL/Scripts/StepCounterMag.cs
+++ Assets/NianticLL/Scripts/StepCounterMag.cs
@@ -9,17 +9,26 @@ public class StepCounterMag : MonoBehaviour
     // Start is called before the first frame update
     public TextMeshProUGUI StepCounterDisplay;
     public TextMeshProUGUI DistCounterDisplay;
+    private Pedometer pedometer;
 
     void Start()
     {
-        var pedometer = new Pedometer(OnStep);
+        pedometer = new Pedometer(OnStep);
+        // Reset UI
+        OnStep(0, 0);
 
-        
     }
 
-    void OnStep(int steps, double distance)
+    private void OnStep(int steps, double distance)
     {
         StepCounterDisplay.text = steps.ToString();
         DistCounterDisplay.text = (distance * 3.28084).ToString("F2") + " ft";
+    }
+
+    private void OnDisable()
+    {
+        // Release the pedometer
+        pedometer.Dispose();
+        pedometer = null;
     }
 }
