dd0b423a7c09738421a02d844e333fb541161e4e
Xinying Chew <x.chew3@gmail.com>
2023-12-02 20:45:47 -0500

    Fix waypoints encounters

--- Assets/NianticLL/Scripts/WaypointController.cs
+++ Assets/NianticLL/Scripts/WaypointController.cs
@@ -9,11 +9,12 @@ public class WaypointController : MonoBehaviour
     public float collisionDistance = 35;
     public float spinSpeed = 10;
     public List<Mesh> npcMesh;
+    public GameObject star;
 
     public int npcChoice;
     private bool inPlayerRadius = false;
-    private float waypointX;
-    private float waypointZ;
+    //private float waypointX;
+    //private float waypointZ;
 
     private Transform waypointObject;
     
@@ -21,8 +22,8 @@ public class WaypointController : MonoBehaviour
     void Start()
     {
         //npcChoice = Random.Range(0, npcMesh.Count);
-        waypointX = transform.position.x;
-        waypointZ = transform.position.z;
+        //waypointX = transform.position.x;
+        //waypointZ = transform.position.z;
         transform.Rotate(0, Random.Range(0, 180), 0);
 
         if (npcChoice == 0)
@@ -57,16 +58,17 @@ public class WaypointController : MonoBehaviour
     {
         transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
 
-        if (distance() < collisionDistance)
+        Debug.Log("Loc for choice num " + npcChoice + ": " + Distance());
+        if (Distance() < collisionDistance)
         {
             if (npcChoice > 0) waypointObject.GetComponent<MeshFilter>().mesh = npcMesh[npcChoice];
-            transform.Find("Star").gameObject.SetActive(true);
+            star.SetActive(true);
             inPlayerRadius = true;
         }
         else
         {
             //waypointObject.GetComponent<MeshFilter>().mesh = npcMesh[0];
-            transform.Find("Star").gameObject.SetActive(false);
+            star.SetActive(false);
             inPlayerRadius = false;
         }
 
@@ -87,10 +89,14 @@ public class WaypointController : MonoBehaviour
 
     }
 
-    private float distance()
+    private float Distance()
     {
-        float xDiff = waypointX - CoverageManager.Inst.NPCPos.x;
-        float zDiff = waypointZ - CoverageManager.Inst.NPCPos.z;
+        float xDiff = transform.position.x - CoverageManager.Inst.NPCPos.x;
+        float zDiff = transform.position.z - CoverageManager.Inst.NPCPos.z;
+
+        //Debug.Log("playerLoc: " + CoverageManager.Inst.NPCPos.x + ", " + CoverageManager.Inst.NPCPos.z);
+        //Debug.Log("sqrtDist: " + Mathf.Sqrt(xDiff * xDiff + zDiff * zDiff));
+
 
         return Mathf.Sqrt(xDiff * xDiff + zDiff * zDiff);
 
