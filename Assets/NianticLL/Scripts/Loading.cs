13171f3d9c6ec1573244029a1966f59e9fa2f249
Xinying Chew <x.chew3@gmail.com>
2023-12-02 21:41:52 -0500

    Update loading progress bar

--- Assets/NianticLL/Scripts/Loading.cs
+++ Assets/NianticLL/Scripts/Loading.cs
@@ -6,6 +6,7 @@ using System.Collections;
 public class Loading : MonoBehaviour
 {
     public Slider progressBar;
+    public float duration = 5f;
 
     void Start()
     {
@@ -21,20 +22,31 @@ public class Loading : MonoBehaviour
         // Don't let the scene activate until we allow it
         asyncOperation.allowSceneActivation = false;
 
-        // Update the progress bar while loading
-        while (!asyncOperation.isDone)
+        //// Update the progress bar while loading
+        //while (!asyncOperation.isDone)
+        //{
+        //    float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f); // 0.9 is the completion value
+        //    progressBar.value = progress;
+
+        //    // Check if the loading is almost complete (0.9 is when it's ready to activate)
+        //    if (asyncOperation.progress >= 0.9f)
+        //    {
+        //        // The scene is ready, let's activate it
+        //        asyncOperation.allowSceneActivation = true;
+        //    }
+
+        //    yield return null;
+        //}
+
+
+        float t = 0.0f;
+        while (t < duration)
         {
-            float progress = Mathf.Clamp01(asyncOperation.progress / 0.9f); // 0.9 is the completion value
+            t += Time.deltaTime;
+            float progress = Mathf.Lerp(0, 1, t / duration);
             progressBar.value = progress;
-
-            // Check if the loading is almost complete (0.9 is when it's ready to activate)
-            if (asyncOperation.progress >= 0.9f)
-            {
-                // The scene is ready, let's activate it
-                asyncOperation.allowSceneActivation = true;
-            }
-
             yield return null;
         }
+        asyncOperation.allowSceneActivation = true;
     }
 }
